8c353de7b4b8c1e034dae5e37f1c9126473043df
Evgeniy kuzmenko <kuzmenko.evgeniy2011@yandex.ru>
2023-07-17 19:08:08 +0300

    add files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // const heart = document.querySelector('.fa-heart__span')
 
-// const basket = document.querySelector('.fa-cart-shopping__basket')
+const basket = document.querySelector('.fa-cart-shopping__basket')
 
 let count_favourites = 1
 
@@ -14,13 +14,27 @@ const favoutires = document.querySelectorAll('.text__favoutires')
 
 const basket_number = document.querySelectorAll('.text__basket')
 
+const minus = document.querySelectorAll('.minus')
+
+const plus = document.querySelectorAll('.plus')
+
+
+
+for(let n of plus){
+    n.addEventListener('click', function(){
+        const volume = document.querySelector('.value').textContent++
+        const basket = document.querySelector('.fa-cart-shopping__basket').textContent++
+    })
+}
+
 
 
 for(let value of favoutires){
     value.addEventListener('click', function(){
         let heart = document.querySelector('.fa-heart__span')
         for(count_favourites; count_favourites > 0; count_favourites++){
-            heart.innerHTML = count_favourites
+            heart.textContent = count_favourites
+            console.log(typeof heart)
             if(heart.textContent == 1){
             }
             break
@@ -28,16 +42,18 @@ for(let value of favoutires){
     })
 }
 
-for(let value1 of basket_number){
-    value1.addEventListener('click', function(){
-        const quantity = document.querySelector('.quantity').textContent--
-        const basket = document.querySelector('.fa-cart-shopping__basket').textContent++
-        for(quantity; quantity < 5;){
-            if(quantity == 1){
-                alert('asda')
-            }
-            break
-        }
-    })
-}
+
+
+// for(let value1 of basket_number){
+//     value1.addEventListener('click', function(){
+//         const quantity = document.querySelector('.quantity').textContent--
+//         const basket = document.querySelector('.fa-cart-shopping__basket').textContent++
+//         for(quantity; quantity < 5;){
+//             if(quantity == 1){
+//                 alert('asda')
+//             }
+//             break
+//         }
+//     })
+// }
 
