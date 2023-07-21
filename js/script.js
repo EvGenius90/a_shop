7108a7ca201e4d2daa6c6354d4e6d56eb2289422
Evgeniy kuzmenko <kuzmenko.evgeniy2011@yandex.ru>
2023-07-21 19:02:31 +0300

    change

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 "use strict";
 
-// const heart = document.querySelector('.fa-heart__span')
-
 const basket = document.querySelector(".fa-cart-shopping__basket");
 const favourites = document.querySelector(".fa-heart__span");
 let value = document.querySelector(".value");
 let search = document.querySelector(".section_2__button");
 let find = document.querySelector(".section_2__find");
+
+find.addEventListener("input", function () {
+  if (find.value != "") {
+    search.textContent = "Искать";
+  } else {
+    search.textContent = "Найти";
+  }
+});
 
 document.querySelector(".plus").addEventListener("click", function () {
   value.textContent++;
@@ -19,9 +25,3 @@ document.querySelector(".minus").addEventListener("click", function () {
     basket.textContent--;
   }
 });
-
-// for(let n of find){
-//     if(n.value == 'a'){
-//         search.textContent = 'Искать'
-//     }
-// }
