fbf8f009396072c2d8bdb752da5b7c81388f24d7
Evgeniy kuzmenko <kuzmenko.evgeniy2011@yandex.ru>
2023-07-24 19:50:18 +0300

    change

--- js/script.js
+++ js/script.js
@@ -2,13 +2,11 @@
 
 const basket = document.querySelector(".fa-cart-shopping__basket");
 const favourites = document.querySelector(".fa-heart__span");
-// let value = document.querySelector(".value");
 let search = document.querySelector(".section_2__button");
 let find = document.querySelector(".section_2__find");
 let logo_support = document.querySelector(".icon-support");
 let support = document.querySelector(".support");
-
-let products = document.querySelector('.set-of-products')
+let products = document.querySelector(".set-of-products");
 
 logo_support.addEventListener("click", function () {
   support.classList.toggle("is-active");
@@ -31,28 +29,39 @@ window.addEventListener("click", function (n) {
     // находим обертку счетчика
     const add_product = n.target.closest(".add-product");
     // карточка товара
-    product = n.target.closest('.section_2__product')
+    product = n.target.closest(".section_2__product");
     // находим div с числом счетчика
     counter = add_product.querySelector("[data-counter]");
   }
   if (n.target.dataset.action == "plus") {
     counter.textContent++;
     basket.textContent++;
-    console.log(product)
-    const product_info = {
-      imgSrc: product.querySelector('.product-img').getAttribute('src'),
-      title: product.querySelector('.product-title').innerText,
-      itemsInBox: product.querySelector('.value').innerText
+
+    let product_info = {
+      id: product.dataset.id,
+      imgSrc: product.querySelector(".product-img").getAttribute("src"),
+      title: product.querySelector(".product-title").innerText,
+      itemsInBox: product.querySelector("[data-counter]").innerText,
+      price: product.querySelector(".price").innerText,
+    };
+
+    let itemInCart = products.querySelector(`[data-id="${product_info.id}"]`);
+    if (itemInCart) {
+      let volueEl = itemInCart.querySelector("[data-count]");
+      volueEl.innerText = "кол-во: " + parseInt(product_info.itemsInBox);
+      console.log(volueEl)
+    } else {
+      const productItem = `
+    <div class="product-inside" data-id="${product_info.id}">
+      <h4 class="product-title">${product_info.title}</h4>
+      <img class="products-images" src="${product_info.imgSrc}" alt="">
+      <p class="product-text" data-count="">${"кол-во: " + product_info.itemsInBox}</p>
+      <p><small>${"цена: " + product_info.price}</small></p>
+    </div>
+    <hr>
+    `;
+      products.insertAdjacentHTML("beforeend", productItem);
     }
-    
-    const productItem = `
-    <h3>${product_info.title}</h3>
-    <img src="${product_info.imgSrc}" alt="">
-    <p>${'кол-во: ' + product_info.itemsInBox}</p>
-    `
-
-    products.insertAdjacentHTML('beforeend', productItem)
-
   }
   if (n.target.dataset.action == "minus") {
     if (
@@ -64,17 +73,3 @@ window.addEventListener("click", function (n) {
     }
   }
 });
-
-// document.querySelector(".plus").addEventListener("click", function () {
-//   value.textContent++;
-//   basket.textContent++;
-//   console.log(value.innerHTML)
-// });
-
-// document.querySelector(".minus").addEventListener("click", function () {
-//   if ((parseInt(value.textContent) > 0) & (parseInt(basket.textContent) > 0)) {
-//     value.textContent--;
-//     basket.textContent--;
-//     console.log(value)
-//   }
-// });
